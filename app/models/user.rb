1e3f49668775933daf1edbdf1ae265899acca2b0
learn <learn@learn02.local>
2016-09-20 16:24:26 -0700

    added has_many relationship to users and apartment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, email: true
+  validates :email, uniqueness: true
+
+  has_many :apartments
 end
